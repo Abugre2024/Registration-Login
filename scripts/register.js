c6d5ab9aac00798844aae07a071f12591b4fb705
Abugre Abayeta <abayetaabugre@gmail.com>
2024-09-17 16:01:35 +0000

    added whatever

--- scripts/register.js
+++ scripts/register.js
@@ -1,8 +1,27 @@
 // Get form element by id
-const registerForm = document.getElementById("register");
-// handle form submit event
-registerForm.onsubmit = function(event) {
-    event.preventDefault();
-    console.log(event);
+const registerForm = document.querySelector("#register");
+console.log(registerForm);
+registerForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // Collect form data
+  const formData = new FormData(registerForm);
+//   console.log(
+//     formData.get("username"),
+//     formData.get(email),
+//     formData.get(password)
+//   );
+// Send data to backend
+// Display success message to user
+const messageH1 = document.querySelector("#message");
+messageH1.textContent = "Accounnt registered successfully!";
+});
 
+// declare variable age
+const age = 71;
+// Define squareAge Function
+function squareAge(age) {
+  return age ** 2;
 }
+
+const squaredAge = squareAge(34);
+console.log(squaredAge);
